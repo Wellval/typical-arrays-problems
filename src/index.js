beffcdec17930a6348af8e8ec0f745564c6f0b8f
Valentina Masalkova <vrassone@gmail.com>
2021-01-07 22:09:45 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(array === undefined || array.length === 0) return 0;
+    array.sort(function(a, b) {
+      return a - b;
+    });
+  return array[0];
+  }
+
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  array.sort(function(a, b) {
+  	return b - a;
+  });
+  return array[0];
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  let sum = 0;
+  let i = 0;
+  while(i < array.length) {
+  	sum += array[i];
+    i++;
+  }
+  return sum / array.length;
 }
+
+
